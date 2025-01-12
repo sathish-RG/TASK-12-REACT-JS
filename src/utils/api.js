78211f5e9719881a9e90f6b936d5894798e7c6d0
sathish <sathish958527@gmail.com>
2025-01-12 12:01:02 +0530

    Update1 api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,6 @@ const API = axios.create({
 
 export const register = (data) => API.post("/register", data);
 export const login = (data) => API.post("/login", data);
-<<<<<<< HEAD
 export const sendOTP = (data) => API.post("/forgot-password", data);
 export const verifyOTP = (data) => API.post("/forgot-password", data);
 export const resetPassword = (data) => API.post("/forgot-password", data);
