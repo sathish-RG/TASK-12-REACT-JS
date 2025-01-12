eddba250b9fa309c6081bc0c95111c52992cb5c2
sathish-RG <sathish958527@gmail.com>
2025-01-12 09:06:54 +0530

    api-update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-12-node-js-3.onrender.com/api/auth",
+  baseURL: "https://task-12-node-js-3.onrender.com/api/auth"
 });
 
-export const register = (data) => API.post("https://task-12-node-js-3.onrender.com/api/auth/register", data);
-export const login = (data) => API.post("https://task-12-node-js-3.onrender.com/api/auth/login", data);
-export const sendOTP = (data) => API.post("https://task-12-node-js-3.onrender.com/forgot-password", data);
-export const verifyOTP = (data) => API.post("https://task-12-node-js-3.onrender.com/forgot-password", data);
-export const resetPassword = (data) => API.post("https://task-12-node-js-3.onrender.com/forgot-password", data);
+export const register = (data) => API.post("/register", data);
+export const login = (data) => API.post("/login", data);
+export const sendOTP = (data) => API.post("/forgot-password/send", data);
+export const verifyOTP = (data) => API.post("/forgot-password/verify", data);
+export const resetPassword = (data) => API.post("/forgot-password/reset", data);
