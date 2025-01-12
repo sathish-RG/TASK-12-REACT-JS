1cc9b490375667fbc3aacb7f50690b283e42d6c1
sathish-RG <sathish958527@gmail.com>
2025-01-12 11:59:47 +0530

    api final

--- src/utils/api.js
+++ src/utils/api.js
@@ -10,8 +10,4 @@ export const login = (data) => API.post("/login", data);
 export const sendOTP = (data) => API.post("/forgot-password", data);
 export const verifyOTP = (data) => API.post("/forgot-password", data);
 export const resetPassword = (data) => API.post("/forgot-password", data);
-=======
-export const sendOTP = (data) => API.post("/forgot-password/send", data);
-export const verifyOTP = (data) => API.post("/forgot-password/verify", data);
-export const resetPassword = (data) => API.post("/forgot-password/reset", data);
->>>>>>> fba059b509f68daf93c9cca5f694859a89f6b06b
+
